801d2cc56e1ccb93663df7e265ad917f211ffe88
Kang Byeonghak <byeonghak.kang@colley.kr>
2023-12-15 11:01:21 +0900

    init

---
+++ TrySwiftUI/TrySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySwiftUIApp.swift
+//  TrySwiftUI
+//
+//  Created by Byeonghak Kang on 12/15/23.
+//
+
+import SwiftUI
+
+@main
+struct TrySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
