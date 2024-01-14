638427622e1619efb35fa7e2aa0ccf90e7ad22d6
ohtwo <hello@tinygram.dev>
2024-01-14 17:27:22 +0900

    feat: add project

---
+++ TrySwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrySwiftUI
+//
+//  Created by Kang Byeonghak on 1/14/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrySwiftUI/TrySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySwiftUIApp.swift
+//  TrySwiftUI
+//
+//  Created by Kang Byeonghak on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct TrySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
