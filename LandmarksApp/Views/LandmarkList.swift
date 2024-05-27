788953384bdefc16296b2cf9e89ec7d8ac24bdd3
ohtwo <hello@tinygram.dev>
2024-05-27 18:08:23 +0900

    feat: ch3 - add favorites

--- LandmarksApp/Views/LandmarkList.swift
+++ LandmarksApp/Views/LandmarkList.swift
@@ -8,15 +8,30 @@
 import SwiftUI
 
 struct LandmarkList: View {
+  @State private var showFavoritesOnly = false
+
+  var filteredLandmarks: [Landmark] {
+    Landmark.landmarks.filter {
+      !showFavoritesOnly || $0.isFavorite
+    }
+  }
+
   var body: some View {
     NavigationSplitView {
-      List(Landmark.landmarks) { landmark in
-        NavigationLink {
-          LandmarkDetail(landmark: landmark)
-        } label: {
-          LandmarkRow(landmark: landmark)
+      List {
+        Toggle(isOn: $showFavoritesOnly) {
+          Text("Favorites only")
+        }
+
+        ForEach(filteredLandmarks) { landmark in
+          NavigationLink {
+            LandmarkDetail(landmark: landmark)
+          } label: {
+            LandmarkRow(landmark: landmark)
+          }
         }
       }
+      .animation(.default, value: filteredLandmarks)
       .navigationTitle("Landmarks")
     } detail: {
       Text("Select a Landmark")
