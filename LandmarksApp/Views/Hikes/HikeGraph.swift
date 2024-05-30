dbf3e0549872f78d3d8feac889a2151706166a5d
ohtwo <hello@tinygram.dev>
2024-05-31 08:21:56 +0900

    feat: ch5 - add transition animation on HikeGraph

--- LandmarksApp/Views/Hikes/HikeGraph.swift
+++ LandmarksApp/Views/Hikes/HikeGraph.swift
@@ -40,6 +40,7 @@ struct HikeGraph: View {
                         range: observation[keyPath: path],
                         overallRange: overallRange
                     )
+                    .animation(.ripple(index: index))
                 }
                 .offset(x: 0, y: proxy.size.height * heightRatio)
             }
@@ -57,6 +58,14 @@ func rangeOfRanges<C: Collection>(_ ranges: C) -> Range<Double>
 
 func magnitude(of range: Range<Double>) -> Double {
     range.upperBound - range.lowerBound
+}
+
+extension Animation {
+  static func ripple(index: Int) -> Animation {
+      Animation.spring(dampingFraction: 0.5)
+        .speed(2)
+        .delay(0.03 * Double(index))
+    }
 }
 
 #Preview {
