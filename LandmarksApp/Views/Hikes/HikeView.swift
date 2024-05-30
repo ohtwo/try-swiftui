d08cf18bb03dd3b17df605551ae698c155136e47
ohtwo <hello@tinygram.dev>
2024-05-31 08:24:07 +0900

    lint

--- LandmarksApp/Views/Hikes/HikeView.swift
+++ LandmarksApp/Views/Hikes/HikeView.swift
@@ -1,66 +1,66 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
+ See the LICENSE.txt file for this sample’s licensing information.
 
-Abstract:
-A view displaying information about a hike, including an elevation graph.
-*/
+ Abstract:
+ A view displaying information about a hike, including an elevation graph.
+ */
 
 import SwiftUI
 
 struct HikeView: View {
-    var hike: Hike
-    @State private var showDetail = false
+  var hike: Hike
+  @State private var showDetail = false
 
-    var body: some View {
-        VStack {
-            HStack {
-                HikeGraph(hike: hike, path: \.elevation)
-                    .frame(width: 50, height: 30)
+  var body: some View {
+    VStack {
+      HStack {
+        HikeGraph(hike: hike, path: \.elevation)
+          .frame(width: 50, height: 30)
 
-                VStack(alignment: .leading) {
-                    Text(hike.name)
-                        .font(.headline)
-                    Text(hike.distanceText)
-                }
-
-                Spacer()
-
-                Button {
-                  withAnimation {
-                    showDetail.toggle()
-                  }
-                } label: {
-                    Label("Graph", systemImage: "chevron.right.circle")
-                        .labelStyle(.iconOnly)
-                        .imageScale(.large)
-                        .rotationEffect(.degrees(showDetail ? 90 : 0))
-//                        .animation(nil, value: showDetail)
-                        .scaleEffect(showDetail ? 1.5 : 1)
-                        .padding()
-//                        .animation(.spring(), value: showDetail)
-                }
-            }
-
-            if showDetail {
-              HikeDetail(hike: hike)
-                .transition(.moveAndFade)
-            }
+        VStack(alignment: .leading) {
+          Text(hike.name)
+            .font(.headline)
+          Text(hike.distanceText)
         }
+
+        Spacer()
+
+        Button {
+          withAnimation {
+            showDetail.toggle()
+          }
+        } label: {
+          Label("Graph", systemImage: "chevron.right.circle")
+            .labelStyle(.iconOnly)
+            .imageScale(.large)
+            .rotationEffect(.degrees(showDetail ? 90 : 0))
+//                        .animation(nil, value: showDetail)
+            .scaleEffect(showDetail ? 1.5 : 1)
+            .padding()
+//                        .animation(.spring(), value: showDetail)
+        }
+      }
+
+      if showDetail {
+        HikeDetail(hike: hike)
+          .transition(.moveAndFade)
+      }
     }
+  }
 }
 
 extension AnyTransition {
-    static var moveAndFade: AnyTransition {
-      .asymmetric(
-        insertion: .move(edge: .trailing).combined(with: .opacity),
-        removal: .scale.combined(with: .opacity))
-    }
+  static var moveAndFade: AnyTransition {
+    .asymmetric(
+      insertion: .move(edge: .trailing).combined(with: .opacity),
+      removal: .scale.combined(with: .opacity))
+  }
 }
 
 #Preview {
-    VStack {
-        HikeView(hike: ModelData().hikes[0])
-            .padding()
-        Spacer()
-    }
+  VStack {
+    HikeView(hike: ModelData().hikes[0])
+      .padding()
+    Spacer()
+  }
 }
