e2ce54406021cbd1eee9e1756c83748322673cc8
ohtwo <hello@tinygram.dev>
2024-06-18 08:52:34 +0900

    wip: add watch app target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Kang Byeonghak on 6/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
